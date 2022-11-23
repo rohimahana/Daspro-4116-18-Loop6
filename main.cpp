2681cb1f1bb961cb6026d409bae45db0c4a8b85f
rohimahana <113038377+rohimahana@users.noreply.github.com>
2022-11-23 07:46:05 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int val, max ,min;
+
+    cout << "Program Ini Akan Berhenti Jika Anda Menginput Angka -99\n" << endl;
+
+    while(true)
+    {
+
+        //input
+        cout << "Masukkan Nilai : ";
+        cin >> val;
+
+        //proses looping akan berhenti ketika x = -99
+        if(val == -99){
+            break;
+        }
+        if(val != -99){
+            if(max < val){
+                max = val;
+            }
+            if(min > val){
+                min = val;
+            }
+        }
+    }
+    cout << endl;
+    cout << "Max = " << max << endl;
+    cout << "Min = " << min << endl;
+}
